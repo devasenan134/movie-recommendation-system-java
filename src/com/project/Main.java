30ec53efadacb06cdd38bf528fea6c27ad7fcfba
devasenan134 <devasenan.murugan@gmail.com>
2021-09-20 18:30:26 +0530

    initial commit

---
+++ src/com/project/Main.java
@@ -0,0 +1,9 @@
+package com.project;
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello world");
+        System.out.println("This is a Movie Review System!!");
+    }
+}
